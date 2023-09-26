124f25b07fea4cb889eb0220ac520015aad23726
Ali Can Yücel <yucelalican30@gmail.com>
2023-09-26 15:24:46 +0300

    ChatApiNetCore7.0
    SignalR,Angular,.Net Core 7.0 Web Api

---
+++ ChatApi/Services/ChatService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace ChatApi.Services
+{
+    public class ChatService
+    {
+        private static readonly Dictionary<string, string> Users = new Dictionary<string,string>();
+        public bool AddUserToLis(string userToAdd)
+        {
+            lock(Users)
+            {
+                foreach(var user in Users)
+                {
+                    if(user.Key.ToLower()==userToAdd.ToLower())
+                    {
+                        return false;
+                    }
+                }
+                Users.Add(userToAdd, null);
+                return true;
+            }
+
+        }
+    }
+}
