1369982d528852536cdbc87b78b3bdd8c75fefa6
alicanyucel <yucelalican@hotmail.com>
2023-10-19 15:06:39 +0300

    deneme commit gün içinde bitiriliyor.

--- ChatApi/Program.cs
+++ ChatApi/Program.cs
@@ -4,7 +4,7 @@ using ChatApi.Services;
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddSingleton<ChatService>();
+builder.Services.AddSingleton<ChatService>(); //service registered.
 builder.Services.AddSignalR();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
