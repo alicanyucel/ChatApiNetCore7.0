3cefab1a6e9581d44d8aa325d9a88e5439f9cc86
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2023-11-24 18:20:35 +0300

    hubs filled

--- ChatApi/Hubs/ChatHub.cs
+++ ChatApi/Hubs/ChatHub.cs
@@ -1,8 +1,24 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using ChatApi.Services;
+using Microsoft.AspNetCore.SignalR;
 
 namespace ChatApi.Hubs
 {
-    public class ChatHub:Hub
+    public class ChatHub: Hub
     {
+        private readonly ChatService _chatService;
+        public ChatHub(ChatService chatService)
+        {
+            _chatService = chatService;
+        }
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId,"Come2Chat");
+            await Clients.Caller.SendAsync("UserConnected");
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId,"Come2Chat");
+            await base.OnDisconnectedAsync(exception);
+        }
     }
 }
